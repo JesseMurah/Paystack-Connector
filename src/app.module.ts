223274abf243a86d593238888c2c0195649134a0
JesseMurah <jkhnmurah@gmail.com>
2025-10-09 09:15:43 +0000

    fix HttpModule error

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PayoutController } from './payout/controller/payout.controller';
+import { PaystackPayoutService } from './payout/service/paystack-payout.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule.forRoot(
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot(
     {
       envFilePath: '.env'
     }
   )],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PayoutController],
+  providers: [AppService, PaystackPayoutService],
 })
 export class AppModule {}
